64f1df1d51796f7af9d09f36c93e7b137ecae2db
Vasilevykh-M <vasilevyh00@gmail.com>
2021-11-03 13:24:14 +0500

    commit

---
+++ color/color/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace color
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
